3b8aa48df7c750b1b37e2c0e53f53ab71db9b08e
Kashfeet Raza <kashfeetraza@gmail.com>
2026-04-13 22:23:54 +0500

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[5] = {12,32,3,31,3};
+    int sum;
+    
+    // for(int i= 0; i < 5; i++){
+    //     cin >> arr[i];
+    // }
+    
+    for(int i= 0; i < 5; i++){
+        sum = arr[0];
+
+        cout << sum;
+    }
+    
+    return 0;
+}
