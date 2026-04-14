c4b17c1e62b6467f2726385f53d52ad4db55e817
Kashfeet Raza <kashfeetraza@gmail.com>
2026-04-14 23:07:36 +0500

    Add files via upload

---
+++ program 2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI = 3.141
+int main ()
+{
+	int radius,Area;
+	printf("enter the radius");
+	scanf("%d",&radius);
+	Area = 3.141*radius*radius;
+	printf("area of circle is %d",Area);
+	getch();
+}
+
