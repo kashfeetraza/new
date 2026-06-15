2242bdf57f08de824d80765e14cb5babc5d99d44
Kashfeet Raza <kashfeetraza@gmail.com>
2026-06-15 22:32:43 +0500

    Add files via upload

---
+++ New88.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+    cout << endl;
+    return 0;
+}
